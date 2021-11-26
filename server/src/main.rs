0531766b0dc4429e061dc5060f9b025fb4c33ea2
0xKLBR <94008063+0xKLBR@users.noreply.github.com>
2021-11-27 00:06:57 +0100

    Update main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,25 +1,35 @@
-// Author : Quentin CHARLES, Nicolas TAHON, Tristan KLIEBER
-// Projet de messagerie instantanée en Rust
+// Author : Quentin CHARLES, Nicolas TAHON, Tristan KLIEBER                                                                                                                                                                                                                          
+// Projet de messagerie instantanée en Rust                                                                                                                                                                                                                                          
+                                                                                                                                                                                                                                                                                     
+use std::thread;                                                                                                                                                                                                                                                                     
+use std::time::Duration;                                                                                                                                                                                                                                                             
+use std::net::{TcpListener, TcpStream};                                                                                                                                                                                                                                              
+use std::sync::mpsc::channel;                                                                                                                                                                                                                                                        
+                                                                                                                                                                                                                                                                                     
+//Adresse d'écoute                                                                                                                                                                                                                                                                   
+const LISTNER_ADDR: &str = "127.0.0.1:9999";                                                                                                                                                                                                                                         
+                                                                                                                                                                                                                                                                                     
+fn main() {                                                                                                                                                                                                                                                                          
+    //lancement du TCPListener et écoute continue du socket                                                                                                                                                                                                                          
+    let server = TcpListener::bind(LISTNER_ADDR).expect("TCPListener échoué");                                                                                                                                                                                                       
+    server.set_nonblocking(true).expect("impossible de mettre la connexion en 'nonblocking'");                                                                                                                                                                                       
+                                                                                                                                                                                                                                                                                     
+    //Initialise client vector macro list
+    let client = vec![];
 
-use std::thread;
-use std::time::Duration;
-use std::io::prelude::*;
 
-use std::net::{TcpListener, TcpStream};
-use std::net::SocketAddr;
-use std::net::TcpStream;
+    //Création de la channel de chat
+    //https://doc.rust-lang.org/std/sync/mpsc/
+    let (tx, rx) = channel();
+    thread::spawn(move|| {
+        tx.send(10).unwrap();
+    });
+    assert_eq!(rx.recv().unwrap(), 10);
+
+    //boucle de réception + affichage des messages
+    loop {
 
 
-const ADDR: &str = "127.0.0.1:666";
-
-
-fn main() {
-    println!("first line");
-    thread::sleep(Duration::from_secs(5));
-
-    if let Ok(stream) = TcpStream::connect("127.0.0.1:8080") {
-        println!("Connected to the server!");
-    } else {
-        println!("Couldn't connect to server...");
     }
+
 }
