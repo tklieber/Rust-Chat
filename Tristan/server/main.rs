05c9b1a6e0d195c09c93997a743abb0e967ca03c
0xKLBR <94008063+0xKLBR@users.noreply.github.com>
2021-12-08 21:00:29 +0100

    Update main.rs

--- Tristan/server/main.rs
+++ Tristan/server/main.rs
@@ -1,56 +1,76 @@
-// Author : Quentin CHARLES, Nicolas TAHON, Tristan KLIEBER                                                                                                                                                                                                                          
-// Projet de messagerie instantanée en Rust                                                                                                                                                                                                                                          
+//Server
 
 use std::thread;
-use std::time::Duration;
 use std::net::{TcpListener, TcpStream};
-use std::sync::mpsc::channel;
+use std::io::{Read, Write, ErrorKind};
+use std::str::from_utf8;
 
 //Adresse d'écoute
 const LISTNER_ADDR: &str = "127.0.0.1:9999";
 
-//fn send_message(&mut String) {
+fn handle_client(mut stream: TcpStream, adresse: &str) {
+    loop {
+        let mut data = [0 as u8; 50]; // buffer 50 bytes
+        let msguser:Vec<u8> = Vec::new();
 
-//}
+
+        while match stream.read(&mut data) {
+            Ok(size) => {
+                //Si 0 octect reçu --> client déconnecté
+                if size < 1 {
+                    println!("client déconnecté {}", adresse);
+                    return
+                }
+                // Ecrit dans la console et send le message au user distant
+                stream.write(&data[0..size]).unwrap();
+
+                let msguser = from_utf8(&data).unwrap();
+                println!("-> L'utilisateur {} à envoyé: {}\n", stream.peer_addr().unwrap(), msguser);
+                true
+            },
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => { false }
+            Err(e) => {
+                println!("Erreur I/O rencontré : {}", e);
+                false
+            },
+        }{}
+    }
+}
+
+
 
 fn main() {
-    //lancement du TCPListener et écoute continue du socket
-    let server = TcpListener::bind(LISTNER_ADDR).expect("TCPListener échoué");
+    let listener = TcpListener::bind(LISTNER_ADDR).expect("TCPListener échoué");
     server.set_nonblocking(true).expect("impossible de mettre la connexion en 'nonblocking'");
-
-    //Initialise client vector macro list
-    let mut client = vec![];
-
-    //Création de la channel de chat
-    //Voir --> https://doc.rust-lang.org/std/sync/mpsc/
-    let (tx, rx) = channel();
-
-    //boucle de réception + affichage des messages
-    loop {
-        //DOC --> https://doc.rust-lang.org/std/net/struct.TcpListener.html
-        if let Ok((mut _socket, addr)) = server.accept() {
-            println!("client {:?} connected !", addr);
-
-            let tx = tx.clone();
-            client.push(_socket.try_clone().expect("impossible de cloner le client dans vec![]"));
+    // accepte les connection, et crée un thread nouveau thread pour chacune d'entre elle
+    println!("écoute sur le port 3333....");
 
 
-            //Envoie de message sur le flux de transmission
-            thread::spawn(move || {
-                let msg = String::from("hello world !");
-                let _sending = match tx.send(msg) {
-                    Ok(sending) => sending,
-                    Err(_error) => panic!("message cannot be send"),
+//watch this -> https://stackoverflow.com/questions/60219160/how-to-store-tcpstream-inside-a-hashmap
+    for stream in listener.incoming() {
+        //vector to store clients
+        let mut clients = Vec::new();
+        match stream {
+            Ok(stream) => {
+                let adresse = match stream.peer_addr() {
+                    Ok(addr) => format!("[adresse : {}]", addr),
+                    Err(_) => "inconnue".to_owned()
                 };
-                thread::sleep(Duration::from_millis(10));
-            });
-            while let Ok(msg) = rx.recv() {
-                println!("{}", msg);
+                println!("Nouvelle connexion: {}", stream.peer_addr().unwrap());
+                clients.push(stream.try_clone().expect("failed to clone client"));
+
+                thread::spawn(move|| {
+                    // connexion réussi
+                    handle_client(stream, &*adresse)
+                });
             }
-
-
-            thread::sleep(Duration::from_millis(10));
+            Err(e) => {
+                println!("Erreur: {}", e);
+                /* connexion échoué */
+            }
         }
-    }
 
+    }
+    // close sockets
+    drop(listener);
 }
