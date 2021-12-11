2bff0d29e9999addb197a2b654add16a8c9178ce
0xKLBR <94008063+0xKLBR@users.noreply.github.com>
2021-12-11 16:49:27 +0100

    Update main.rs

--- Tristan/client/main.rs
+++ Tristan/client/main.rs
@@ -1,9 +1,109 @@
-//Client
+#![allow(unused_variables)]
+#![allow(unused_imports)]
 
-use std::net::{TcpStream};
-use std::io::{Read, Write};
-use std::str::from_utf8;
+use std::env;
+use std::process;
+use std::thread;
+use std::io::{self, Read, Write, Error};
+use std::net::TcpStream;
+use std::net::TcpListener;
+
+type Port = u16;
+
+struct Program {
+    name: String
+}
+
+impl Program {
+    fn new(name: String) -> Program {
+        Program { name: name }
+    }
+
+    fn usage(&self) {
+        println!("usage: {} HOST PORT",self.name);
+    }
+
+    fn print_error(&self,mesg: String) {
+        writeln!(io::stderr(),"{}: error: {}",self.name,mesg);
+    }
+
+    fn print_fail(&self,mesg: String) -> ! {
+        self.print_error(mesg);
+        self.fail();
+    }
+
+    fn exit(&self,status: i32) -> ! { process::exit(status); }
+    fn fail(&self) -> ! { self.exit(-1); }
+}
+
+fn main() {
+    let mut args = env::args();
+    let program = Program::new(
+        args.next().unwrap_or("test".to_string())
+    );
+
+    let host = args.next().unwrap_or_else(|| {
+        program.usage();
+        program.fail();
+    });
+
+    let port = args.next().unwrap_or_else(|| {
+        program.usage();
+        program.fail();
+    }).parse::<Port>().unwrap_or_else(|error| {
+        program.print_error(format!("invalid port number: {}",error));
+        program.usage();
+        program.fail();
+    });
+
+    let mut stream = TcpStream::connect(
+        (host.as_str(), port)
+    ).unwrap_or_else(|error|
+        program.print_fail(error.to_string())
+    );
+    let mut input_stream = stream.try_clone().unwrap();
+
+    let handler = thread::spawn(move || {
+        let mut client_buffer = vec![0u8; 1024];
+
+        loop {
+            match input_stream.read(&mut client_buffer) {
+                Ok(n) => {
+                    if n == 0 {
+                        program.exit(0);
+                    }
+                    else
+                    {
+                        io::stdout().write(&client_buffer).unwrap();
+                        io::stdout().flush().unwrap();
+                    }
+                },
+                Err(error) => program.print_fail(error.to_string()),
+            }
+        }
+    });
+
+    let output_stream = &mut stream;
+    let mut user_buffer = String::new();
+
+    loop {
+        io::stdin().read_line(&mut user_buffer).unwrap();
+
+        output_stream.write(user_buffer.as_bytes()).unwrap();
+        output_stream.flush().unwrap();
+        user_buffer.clear();
+    }
+}
+
+/*
+use std::sync::thread;
 use std::io;
+use tokio::{
+    net::TcpStream,
+    io::{AsyncWriteExt,AsyncBufReadExt, BufReader},
+};
+
+const SERVER_ADDR: &str = "127.0.0.1:9999";
 
 
 fn user_input() -> String {
@@ -19,25 +119,126 @@ fn user_input() -> String {
                     buffer.pop();
                 }
             }
-            println!("> entré : {}", buffer)
+            println!("> entré : {}", buffer);
+            return buffer
         },
         Err(e) => println!("Une erreur est survenue : {}", e)
     }
     buffer
 }
 
+
+/*
+//a adapter
 fn send_and_receive_msg(mut stream: &TcpStream){
     loop{
         let input: String = user_input();
         let msg = input.as_bytes();
         let msglen = msg.len();
 
-        stream.write(msg).expect("erreur lors de l'envoie du message dans le stream");
+        stream.write(msg);
         print!("Message envoyé, en attente de la réponse.....");
 
         //let mut data = [0 as u8; 25]; // buffer 6 bytes
         let mut data: Vec<u8> = vec![0; msglen];
 
+        /*
+        PLUS NÉCESSAIRE -> UN SIMPLE READ SUFFIT
+        match stream.read_exact(&mut data) {
+            // A MODIFIER !!! -> ne renvoie pas la rep
+            Ok(_) => {
+                if &data == msg {
+                    println!("Message reçu!");
+                } else {
+                    let text = from_utf8(&data).unwrap();
+                    println!("Réponse unexpected: {}", text);
+                }
+            },
+            Err(e) => {
+                println!("Erreur dans la réception des données: {}", e);
+            }
+        }
+        */
+    }
+}
+*/
+#[tokio::main]
+async fn main() {
+    let mut connection = TcpStream::connect(SERVER_ADDR).await.unwrap();
+    println!("connected");
+
+    let (reader, mut writer) =  connection.split();
+    let mut reader = BufReader::new(reader);
+
+
+    //USELESS
+    let mut addr = &connection.local_addr().unwrap();
+    println!("{}",addr);
+
+    let mut recved_data = String::new();
+
+    tokio::spawn(async move {
+        loop{
+            let input: String = user_input();
+
+                    thread::select! {
+                        //lit la réception de TCPListener et l'affiche = affiche les msg entrant
+                        resultat_select = reader.read_line(&mut recved_data) => {
+                            //si on reçoit rien -> break
+                            if resultat_select.unwrap() == 0 { return }
+
+                            println!("your address{}",addr);
+                            //println!("{}", resultat_select);
+                            recved_data.clear();
+                        }
+                        //Lit et envoie stdin = affiche et envoie le stdin
+                        resultat_select = writer.write_all(input.as_bytes()).await.unwrap() => {
+                            match resultat_select {
+                                Ok(_) => { println!("send") },
+                                Err(e) => {println!("Une Erreur est survenue : {}", e)}
+                            }
+                            println!("ok");
+                        }
+                    }
+            }
+        });
+}
+
+/*
+----------------------------------------------
+                OLD
+----------------------------------------------
+use std::net::{TcpStream};
+use std::io::{Read, Write};
+use std::str::from_utf8;
+use std::io;
+fn user_input() -> String {
+    let mut buffer = String::new();
+    let stdin = io::stdin();
+    match stdin.read_line(&mut buffer){
+        Ok(_) => {
+            //Enleve la touche 'Entrer'
+            if buffer.ends_with('\n') {
+                buffer.pop();
+                if buffer.ends_with('\r') {
+                    buffer.pop();
+                }
+            }
+            println!("> entré : {}", buffer)
+        },
+        Err(e) => println!("Une erreur est survenue : {}", e)
+    }
+    buffer
+}
+fn send_and_receive_msg(mut stream: &TcpStream){
+    loop{
+        let input: String = user_input();
+        let msg = input.as_bytes();
+        let msglen = msg.len();
+        stream.write(msg).expect("erreur lors de l'envoie du message dans le stream");
+        print!("Message envoyé, en attente de la réponse.....");
+        //let mut data = [0 as u8; 25]; // buffer 6 bytes
+        let mut data: Vec<u8> = vec![0; msglen];
         match stream.read_exact(&mut data) {
             Ok(_) => {
                 if &data == msg {
@@ -55,9 +256,8 @@ fn send_and_receive_msg(mut stream: &TcpStream){
         }
     }
 }
-
 fn main() {
-    match TcpStream::connect("localhost:3333") {
+    match TcpStream::connect("localhost:9999") {
         Ok(mut stream) => {
             println!("Connexion réussi sur le port 3333 !");
             send_and_receive_msg(&mut stream);
@@ -65,6 +265,8 @@ fn main() {
         Err(e) => {
             println!("Erreur de connexion: {}", e);
         }
-            }   
+            }
     println!("Connexion terminé");
 }
+*/
+*/
