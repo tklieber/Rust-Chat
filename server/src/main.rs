6893e45af3b703407ef5b5b7e789cedda436cc20
0xKLBR <94008063+0xKLBR@users.noreply.github.com>
2021-12-12 18:31:14 +0100

    Update main.rs

--- server/src/main.rs
+++ server/src/main.rs
@@ -1,15 +1,12 @@
 //Server
 
-//use std::io::{Read, Write, ErrorKind};
 use tokio::{sync::broadcast, net::TcpListener, io::BufReader};
 use tokio::io::AsyncWriteExt;
 use tokio::io::AsyncBufReadExt;
 
 
-
 //Adresse d'écoute
 const LISTNER_ADDR: &str = "127.0.0.1:9999";
-
 
 
 #[tokio::main]
@@ -20,30 +17,39 @@ async fn main() {
     let (tx, _rx) = broadcast::channel(10);
 
     loop {
-        //Accept les clients qui viennent sur le TCPStream
+        //Accepte les clients qui viennent sur le TCPStream
         let (mut socket, addr) = listener.accept().await.unwrap();
 
         let tx = tx.clone();
         let mut rx = tx.subscribe();
-        println!("Nouvelle connexion: {}\n", addr);
+        println!("Nouvelle connexion: {}", addr);
 
         tokio::spawn(async move {
-            //handle_client(stream, &*adresse, &tx, &rx);
             //nécessité de split le socket pour pouvoir écouter et écrire en même temps
             let (reading, mut writing) = socket.split();
 
             let mut reading = BufReader::new(reading);
             let mut recved_data = String::new();
-            // buffer -> Obligé que ça soit un String car .clone()
 
             loop {
                 //permet de lire et envoyer en même temps, un peu comme thread
                 tokio::select! {
                     resultat_select = reading.read_line(&mut recved_data) => {
-                        //si on reçoit rien -> break
-                        if resultat_select.unwrap() == 0 { break }
+                        //si on ne reçoit rien alors ça romp le TCPStream
+                        if resultat_select.unwrap() == 0 {
+                            println!("Utilisateur {} s'est déconnecté", addr);
+                            break
+                        }
+                        //si l'user entre "quit" alors ça romp le TCPStream
+                        if recved_data == String::from(":quit\n"){
+                            println!("Utilisateur {}, s'est déconnecté", addr);
+                            break
+                        }
 
-                        tx.send((recved_data.clone(), addr)).unwrap();
+                        tx.send((recved_data.clone(), addr)).unwrap(); //-> rx
+                        //on enlève la touche "entré"
+                        //if recved_data.ends_with('\n') { recved_data.pop(); if recved_data.ends_with('\r') { recved_data.pop();}}
+
                         println!("{} à écrit : {}",addr, recved_data);
                         recved_data.clear();
                     }
@@ -56,3 +62,71 @@ async fn main() {
                         }
                     }
                 }
+
+
+                /*
+                match stream.read(&mut data) {
+                    Ok(size) => {
+                        //Si 0 octect reçu --> client déconnecté
+                        if size < 1 {
+                            println!("client {} déconnecté", adresse);
+                            return
+                        }
+                        /*
+                        // Ecrit dans la console et send le message au user distant
+                        // OLD -> stream.write_all(&data[0..size]).unwrap();
+                        tx.send(stream.read(&mut data).unwrap());
+                        stream.write_all(&data[0..size]).unwrap();
+                        let msguser = from_utf8(&data).unwrap();
+                        println!("-> L'utilisateur {} à envoyé: {}\n", stream.peer_addr().unwrap(), msguser);
+                        true
+                         */
+                    },
+                    Err(ref e) if e.kind() == ErrorKind::WouldBlock => { return }
+                    Err(e) => {
+                        println!("Erreur I/O rencontré : {}", e);
+                        return
+                    },
+                }{}
+                 */
+
+            }
+        });
+    }
+}
+/*
+fn handle_client(mut stream: TcpStream, adresse: &str, tx: &dyn Send, rx: &dyn Send) {
+    loop {
+        let mut data = [0 as u8; 50]; // buffer 50 bytes
+        match stream.read(&mut data) {
+            Ok(size) => {
+                //Si 0 octect reçu --> client déconnecté
+                if size < 1 {
+                    println!("client {} déconnecté", adresse);
+                    return
+                }
+                /*
+                // Ecrit dans la console et send le message au user distant
+                // OLD -> stream.write_all(&data[0..size]).unwrap();
+                tx.send(stream.read(&mut data).unwrap());
+                stream.write_all(&data[0..size]).unwrap();
+                let msguser = from_utf8(&data).unwrap();
+                println!("-> L'utilisateur {} à envoyé: {}\n", stream.peer_addr().unwrap(), msguser);
+                true
+                 */
+            },
+            Err(ref e) if e.kind() == ErrorKind::WouldBlock => { return }
+            Err(e) => {
+                println!("Erreur I/O rencontré : {}", e);
+                return
+            },
+        }{}
+        // Ecrit dans la console et send le message au user distant
+        // OLD -> stream.write_all(&data[0..size]).unwrap();
+        tx.send(stream.read(&mut data).unwrap());
+        stream.write_all(&data).unwrap();
+        let msguser = from_utf8(&data).unwrap();
+        println!("-> L'utilisateur {} à envoyé: {}\n", stream.peer_addr().unwrap(), msguser);
+    }
+}
+*/
