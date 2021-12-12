3c261458a970c94477adc110c9eb74027777bfe4
0xKLBR <94008063+0xKLBR@users.noreply.github.com>
2021-12-12 18:29:45 +0100

    Update main.rs

--- Tristan/client/main.rs
+++ Tristan/client/main.rs
@@ -1,6 +1,7 @@
 #![allow(unused_variables)]
 #![allow(unused_imports)]
 
+use openssl::base64::{decode_block, encode_block};
 use std::env;
 use std::process;
 use std::thread;
@@ -8,10 +9,17 @@ use std::io::{self, Read, Write, Error};
 use std::net::TcpStream;
 use std::net::TcpListener;
 
-type Port = u16;
+
+const SERVER_ADDR:&str = "127.0.0.1:9999";
+
 
 struct Program {
     name: String
+}
+
+fn encrypt (user_buffer: &mut String) -> String {
+    let user_buffer_encrypted = encode_block(user_buffer.as_bytes() as &[u8]);
+    user_buffer_encrypted
 }
 
 impl Program {
@@ -19,12 +27,8 @@ impl Program {
         Program { name: name }
     }
 
-    fn usage(&self) {
-        println!("usage: {} HOST PORT",self.name);
-    }
-
     fn print_error(&self,mesg: String) {
-        writeln!(io::stderr(),"{}: error: {}",self.name,mesg);
+        println!("{}: error: {}",self.name,mesg);
     }
 
     fn print_fail(&self,mesg: String) -> ! {
@@ -36,45 +40,31 @@ impl Program {
     fn fail(&self) -> ! { self.exit(-1); }
 }
 
+
 fn main() {
     let mut args = env::args();
     let program = Program::new(
         args.next().unwrap_or("test".to_string())
     );
 
-    let host = args.next().unwrap_or_else(|| {
-        program.usage();
-        program.fail();
-    });
-
-    let port = args.next().unwrap_or_else(|| {
-        program.usage();
-        program.fail();
-    }).parse::<Port>().unwrap_or_else(|error| {
-        program.print_error(format!("invalid port number: {}",error));
-        program.usage();
-        program.fail();
-    });
-
-    let mut stream = TcpStream::connect(
-        (host.as_str(), port)
-    ).unwrap_or_else(|error|
+    let mut stream = TcpStream::connect(SERVER_ADDR).unwrap_or_else(|error|
         program.print_fail(error.to_string())
     );
     let mut input_stream = stream.try_clone().unwrap();
 
     let handler = thread::spawn(move || {
-        let mut client_buffer = vec![0u8; 1024];
+        let mut client_buffer = vec![0u8];
 
         loop {
             match input_stream.read(&mut client_buffer) {
                 Ok(n) => {
                     if n == 0 {
+                        println!("Connexion avec le serveur interrompu !");
                         program.exit(0);
                     }
                     else
                     {
-                        io::stdout().write(&client_buffer).unwrap();
+                        io::stdout().write_all(&client_buffer).unwrap();
                         io::stdout().flush().unwrap();
                     }
                 },
@@ -87,9 +77,13 @@ fn main() {
     let mut user_buffer = String::new();
 
     loop {
+
         io::stdin().read_line(&mut user_buffer).unwrap();
 
-        output_stream.write(user_buffer.as_bytes()).unwrap();
+        let user_buffer_encrypted: String = encrypt(&mut user_buffer);
+        println!("{} -> message chiffré", user_buffer_encrypted);
+
+        output_stream.write_all(user_buffer_encrypted.as_bytes()).unwrap();
         output_stream.flush().unwrap();
         user_buffer.clear();
     }
