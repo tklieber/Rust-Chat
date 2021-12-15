c83c16948340928b09ccb79367a9ced8fa742e5e
amTeaq <76106120+amTeaq@users.noreply.github.com>
2021-12-15 22:16:29 +0100

    Update main.rs

--- Quentin/client/main.rs
+++ Quentin/client/main.rs
@@ -1,77 +1,123 @@
-//Client
-
-use std::net::{TcpStream};
-use std::io::{Read, Write};
-use std::str::from_utf8;
-use std::io;
+#![allow(unused_variables)]
 
 
+use std::env;
+use std::process;
+use std::thread;
+use std::io::{self, Read, Write};
+use std::net::TcpStream;
+use aes::Aes128;
+use block_modes::{BlockMode, Cbc};
+use block_modes::block_padding::Pkcs7;
+use hex_literal::hex;
 
-fn user_input() -> String {
-    let mut buffer = String::new();
-    let stdin = io::stdin();
-    match stdin.read_line(&mut buffer){
-        Ok(_) => {
-            println!("entrer: {}", buffer);
-        },
-        Err(e) => println!("Une erreur est survenue : {}", e)
+const SERVER_ADDR:&str = "127.0.0.1:9999";
+
+
+struct Program {
+    name: String
+}
+/*
+fn encrypt (user_buffer: &mut String, cipher: Cbc<Aes128, Pkcs7>) -> &[u8] {
+    let user_buffer_encrypted = cipher.encrypt_vec(user_buffer as [u8]);
+    user_buffer_encrypted
+}
+fn decrypt (user_buffer_encrypted : &mut String, cipher: Cbc<Aes128, Pkcs7>) -> &[u8] {
+    let user_buffer_decrypted = cipher.decrypt_vec(&user_buffer_encrypted).unwrap();
+    user_buffer_decrypted
+}
+ */
+
+fn dechiffrer (client_buffer: &[u8]) -> String{
+    //cypher variables
+    //----------------
+    type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+    let key = hex!("000102030405060708090a0b0c0d0e0f");
+    let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
+    let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
+    //---------------
+    let user_buffer_uncrypted = cipher.decrypt_vec(client_buffer).unwrap();
+    let prefinal_buffer: &[u8] = &user_buffer_uncrypted;
+    let stdout_string:&str = std::str::from_utf8(&prefinal_buffer).unwrap();
+    stdout_string.to_string()
+}
+
+impl Program {
+    fn new(name: String) -> Program {
+        Program { name: name }
     }
 
-    return buffer;
+    fn print_error(&self,mesg: String) {
+        println!("{}: error: {}",self.name,mesg);
+    }
+
+    fn print_fail(&self,mesg: String) -> ! {
+        self.print_error(mesg);
+        self.fail();
+    }
+
+    fn exit(&self,status: i32) -> ! { process::exit(status); }
+    fn fail(&self) -> ! { self.exit(-1); }
 }
 
 
-
 fn main() {
-    match TcpStream::connect("localhost:3333") {
-        Ok(mut stream) => {
-            println!("Connexion réussi sur le port 3333");
-/*
-            let mut buffer = String::new();
-            let mut stdin = io::stdin();
-            stdin.read_line(&mut buffer);
-            println!("entrer: {}", buffer)?;
-            Ok(())
-*/
+    let mut args = env::args();
+    let program = Program::new(
+        args.next().unwrap_or("test".to_string())
+    );
 
+    let mut stream = TcpStream::connect(SERVER_ADDR).unwrap_or_else(|error|
+        program.print_fail(error.to_string())
+    );
+    let mut input_stream = stream.try_clone().unwrap();
 
+    let handler = thread::spawn(move || {
+        let mut client_buffer: &mut [u8] = &mut [0u8;2048];
 
-
-
-            loop{
-            let input: String;
-            input = user_input();
-            let msg = input.as_bytes();
-            let msglen = msg.len();
-           //let msg = buffer.as_bytes();
-            //let msg = b"Ping!!";
-
-            stream.write(msg).unwrap();
-            println!("Message envoyé, en attente de la réponse...");
-
-            //let mut data = [0 as u8; 25]; // buffer 6 bytes
-            let mut data: Vec<u8> = vec![0; msglen];
-            match stream.read_exact(&mut data) {
-                Ok(_) => {
-                    if &data == msg {
-                        println!("Message reçu!");
-                        let msgreceive = from_utf8(&data).unwrap();
-                        println!("le message reçu est {}", msgreceive);
-
-                    } else {
-                        let text = from_utf8(&data).unwrap();
-                        println!("Réponse unexpected: {}", text);
+        loop {
+            match input_stream.read(&mut client_buffer) {
+                Ok(n) => {
+                    if n == 0 {
+                        println!("Connexion avec le serveur interrompu !");
+                        program.exit(0);
+                    }
+                    else
+                    {
+                        let new_slice = &client_buffer[0..n];
+                        let printed_strings = dechiffrer(&new_slice);
+                        println!("{}",printed_strings);
+                        io::stdout().flush().unwrap();
                     }
                 },
-                Err(e) => {
-                    println!("Erreur dans la réception des données: {}", e);
-                }
+                Err(error) => program.print_fail(error.to_string()),
             }
-            }
-        },
-        Err(e) => {
-            println!("Erreur de connexion: {}", e);
         }
-            }   
-    println!("Connexion terminé");
+    });
+
+    let output_stream = &mut stream;
+    let mut user_buffer = String::new();
+
+    loop {
+
+        io::stdin().read_line(&mut user_buffer).unwrap();
+
+        /*
+        -----------------
+        CIPHER TEST
+        -----------------
+        */
+        type Aes128Cbc = Cbc<Aes128, Pkcs7>;
+        let key = hex!("000102030405060708090a0b0c0d0e0f");
+        let iv = hex!("f0f1f2f3f4f5f6f7f8f9fafbfcfdfeff");
+        let cipher = Aes128Cbc::new_from_slices(&key, &iv).unwrap();
+        //encrypt the data
+        let user_buffer_encrypted = cipher.encrypt_vec(user_buffer.as_bytes());
+        //  Vec<u8>  ->  &[u8]
+        let final_sent_buffer:&[u8] = &user_buffer_encrypted;
+        //-----------> on envoie un &[u8]
+        output_stream.write_all(final_sent_buffer).unwrap();
+        output_stream.flush().unwrap();
+        user_buffer.clear();
+    }
 }
